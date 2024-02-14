0ea7bed24d1a0313ad48dffafaae9187f79c4f0f
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-02-14 16:47:27 -0500

    aprendiendo ciclos js

---
+++ script.js
@@ -0,0 +1,27 @@
+/*Ciclo for */
+
+// for (var index = 1; index <10 ; index++) {
+//     let nombre = prompt ("Ingrese su nombre:");
+//     console.log (nombre+index)
+    
+// }
+
+
+// var numero=Number(prompt("Ingrese numero a multiplicar"))
+
+// for (let index = 1; index <= 10; index++) {
+//     console.log(numero +' * '+index+' = '+(numero*index))
+    
+// }
+
+// var numero=1
+// while(numero<=10){
+//     console.log('esto esun ciclo mientras')
+//     numero++
+// }
+
+
+var numero=1
+do{ console.log('esto esun ciclo mientras')
+   numero++ 
+} while(numero<=10)
